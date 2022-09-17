9c4b91bd8a15bdcf99f90003649021f3d4d8cd8d
Katherine Negrete <katherine.negrete.aguilar@gmail.com>
2022-09-16 20:45:42 -0500

    content(readme): add filter, map and forEach

---
+++ filter()/filterNumbers.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4, 5, 6, , 7, 8, 9];
+const filterNumbers = numbers.filter((x) => x < 5);
+console.log(filterNumbers);
---
+++ map()/givenArray.js
@@ -0,0 +1,5 @@
+const givenArray = ["pen", "book", "code"];
+const newArray = givenArray.map((w) => `${w}s`);
+
+console.log(givenArray);
+console.log(newArray);
