2f606e14a5326774850c53017d552d2e8ea4ac4d
Katherine Negrete <katherine.negrete.aguilar@gmail.com>
2022-09-18 19:18:50 -0500

    content(code): update

--- filter()/filterNumbers.js
+++ filter()/filterNumbers.js
@@ -1,3 +1,7 @@
 const numbers = [1, 2, 3, 4, 5, 6, , 7, 8, 9];
+
 const filterNumbers = numbers.filter((value) => value < 5);
 console.log(filterNumbers);
+
+const error = numbers.filter((value) => value + 2);
+console.log(error);
